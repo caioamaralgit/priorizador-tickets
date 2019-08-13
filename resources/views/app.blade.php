7d442aee4215f7e267249e41ee0b9002459d3f33
Caio Amaral <cbarreto_sud@hotmail.com>
2019-08-12 21:46:17 -0300

    Starting project

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>Priorizador de Tickets</title>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
+    </head>
+    <body>
+        <h1>Priorizador de Tickets</h1>
+    </body>
+</html>
