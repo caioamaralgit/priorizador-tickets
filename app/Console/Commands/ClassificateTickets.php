ea156c38b5e4926fb86a8570826033972007892b
Caio Amaral <cbarreto_sud@hotmail.com>
2019-08-12 23:03:34 -0300

    Defined output argument

--- app/Console/Commands/ClassificateTickets.php
+++ app/Console/Commands/ClassificateTickets.php
@@ -6,8 +6,13 @@ use Illuminate\Console\Command;
 
 class ClassificateTickets extends Command
 {
-    protected $signature = 'classificate {file}';
+    protected $signature = 'classificate 
+        {file : Caminho absoluto do arquivo JSON a ser classificado.} 
+        {output? : Diretório onde o novo JSON deve ser colocado. Caso nulo o programa assumirá o diretório do arquivo de entrada.}';
+
     protected $description = 'Classifica os tickets do arquivo indicado.';
+    protected $file;
+    protected $output;
 
     public function __construct()
     {
@@ -16,6 +21,15 @@ class ClassificateTickets extends Command
 
     public function handle()
     {
-        echo "Iniciando classificação dos tickets no arquivo '" . $this->argument('file') . "'";
+        $this->file = $this->argument('file');
+        $this->output = $this->defineOutput($this->argument('output'));
+        
+        echo "-> Iniciando classificação dos tickets no arquivo '" . $this->file . "'";
+    }
+    
+    protected function defineOutput($output) {
+        if ($output != "") return $output;
+        
+        return substr($this->file, 0, strrpos($this->file, '/')); // Captura o caminho do arquivo até a última "/"
     }
 }
